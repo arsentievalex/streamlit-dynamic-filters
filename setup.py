89725335c975efdcb2eacbaf63b0786e0e74aa0b
Oleksandr Arsentiev <108185255+arsentievalex@users.noreply.github.com>
2023-09-28 18:34:20 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,24 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.1'
+VERSION = '0.1.3'
 DESCRIPTION = "Dynamic multiselect filters for Streamlit"
-LONG_DESCRIPTION = 'This custom components allows users to create dynamic multiselect filters in Streamlit.'
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    readme = fh.read()
+
+with open("CHANGELOG.md", "r", encoding="utf-8") as fh:
+    changelog = fh.read()
 
 setup(
-    name="streamlit-dynamic-filters",
+    name="streamlit_dynamic_filters",
     version=VERSION,
     author="Oleksandr Arsentiev",
     author_email="<arsentiev9393@gmail.com>",
     description=DESCRIPTION,
-    long_description=LONG_DESCRIPTION,
+    long_description=f"{readme}\n\n{changelog}",
+    long_description_content_type="text/markdown",
     packages=find_packages(),
-    install_requires=['streamlit', 'pandas'],
+    install_requires=['streamlit'],
     keywords=['streamlit', 'custom', 'component'],
     license="MIT",
     url="https://github.com/arsentievalex/streamlit-dynamic-filters",
